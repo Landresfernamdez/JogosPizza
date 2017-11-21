37f28f66c2b1601c8cdf55ad8b27c0c94cc42b8e
Andresf12 <landresf3638@gmail.com>
2017-11-21 00:40:07 -0600

    correcciones a la base de datos

--- DB/JogosDB.sql
+++ DB/JogosDB.sql
@@ -1,7 +1,6 @@
 ﻿
 ----tabla de los clientes
 CREATE  TABLE clientes (
-
 	nombre_usuario VARCHAR(50) NOT NULL,
 	nombre VARCHAR(50) NOT NULL,
 	apellido1 VARCHAR(50) NOT NULL,
@@ -11,100 +10,74 @@ CREATE  TABLE clientes (
 	provincia VARCHAR(50) NOT NULL,
 	detalle VARCHAR(50) NOT NULL,
 	contrasena VARCHAR(50) NOT NULL,
-
 	PRIMARY KEY (nombre_usuario)
 );
 
 ----tabla de los compradores
 CREATE  TABLE comprador (
-
 	nombre_usuario VARCHAR(50) NOT NULL,
 	id_comprador SERIAL NOT NULL,
-
 	PRIMARY KEY (nombre_usuario),
-	FOREIGN KEY  (nombre_usuario) REFERENCES clientes (nombre_usuario)
+	CONSTRAINT FK_nombre_usuario_clientes_comprador FOREIGN KEY  (nombre_usuario) REFERENCES clientes (nombre_usuario)
 );
-
 ----tabla del administrador 
-
 CREATE  TABLE administrador (
-
 	nombre_usuario VARCHAR(50) NOT NULL,
 	id_administrador SERIAL NOT NULL,
 	puesto VARCHAR(30) NOT NULL,
-
 	PRIMARY KEY (nombre_usuario),
-	FOREIGN KEY  (nombre_usuario) REFERENCES clientes (nombre_usuario)
+	CONSTRAINT FK_nombre_usuario_clientes_administrador FOREIGN KEY (nombre_usuario) REFERENCES clientes (nombre_usuario)
 );
-
-----tabla de los productos
-CREATE  TABLE productos (
-
-	nombre_producto VARCHAR(50) NOT NULL,
-	precio MONEY NOT NULL,
-	estado INT NOT NULL,
-
-	PRIMARY KEY (nombre_producto)
-);
-
-
 ----tabla de las categorias
 CREATE  TABLE categorias (
-	
 	nombre_categoria VARCHAR(50) NOT NULL,  
 	PRIMARY KEY (nombre_categoria)
 );
-
-
+----tabla de los productos
+CREATE  TABLE productos (
+	nombre_producto VARCHAR(50) NOT NULL,
+	precio MONEY NOT NULL,
+	estado INT NOT NULL,
+	nombre_categoria VARCHAR(50) NOT NULL,
+	PRIMARY KEY (nombre_producto),
+	CONSTRAINT FK_nombre_categoria_categorias_productos FOREIGN KEY  (nombre_categoria) REFERENCES categorias (nombre_categoria)
+)
+----tabla del administrador y comprador
+CREATE TABLE AC(
+	nombre_usuario VARCHAR(50) NOT NULL,
+	nombre_categoria VARCHAR(50) NOT NULL,
+	PRIMARY KEY (nombre_usuario,nombre_categoria),
+	CONSTRAINT FK_nombre_usuario_administradores_categorias FOREIGN KEY  (nombre_usuario) REFERENCES administrador (nombre_usuario),
+	CONSTRAINT FK_nombre_categoria_categoria_administradores FOREIGN KEY  (nombre_categoria) REFERENCES categorias (nombre_categoria)
+);
 ----tabla de los pedidos
 CREATE  TABLE pedido(
-
 	id_pedido SERIAL NOT NULL,
 	estado CHAR(1) NOT NULL,
 	fecha DATE NOT NULL,
 	nombre_usuario VARCHAR(50) NOT NULL,
-    
 	PRIMARY KEY (id_pedido),
-	FOREIGN KEY  (nombre_usuario) REFERENCES comprador (nombre_usuario)
+	CONSTRAINT FK_nombre_usuario_comprador_pedido FOREIGN KEY  (nombre_usuario) REFERENCES comprador (nombre_usuario)
 );
-
-
-----tabla del administrador y comprador
-CREATE TABLE AC (
-
-    nombre_usuario VARCHAR(50) NOT NULL,
-    nombre_categoria VARCHAR(50) NOT NULL,
-	
-	PRIMARY KEY (nombre_usuario),
-	FOREIGN KEY  (nombre_usuario) REFERENCES administrador (nombre_usuario),
-	FOREIGN KEY  (nombre_categoria) REFERENCES categorias (nombre_categoria)
-);
-
 ----tabla del administrador y del pedido
 CREATE  TABLE AP (
-
 	id_pedido SERIAL NOT NULL,
 	nombre_usuario VARCHAR(50) NOT NULL,
-
-	FOREIGN KEY  (id_pedido) REFERENCES pedido (id_pedido),
-	FOREIGN KEY  (nombre_usuario) REFERENCES administrador (nombre_usuario)
+	CONSTRAINT FK_id_pedido_pedido_administrador FOREIGN KEY  (id_pedido) REFERENCES pedido (id_pedido),
+	CONSTRAINT FK_nombre_usuario_administrador_pedido FOREIGN KEY  (nombre_usuario) REFERENCES administrador (nombre_usuario)
 );	
 
 ----tabla entre pedidp y producto
 CREATE  TABLE PP (
-
 	nombre_producto VARCHAR(50) NOT NULL,
 	id_pedido SERIAL NOT NULL,
 	cantidad INT NOT NULL,
-
-	FOREIGN KEY  (nombre_producto) REFERENCES productos (nombre_producto),
-	FOREIGN KEY  (id_pedido) REFERENCES pedido (id_pedido)
+	CONSTRAINT FK_nombre_producto_producto_pedido FOREIGN KEY  (nombre_producto) REFERENCES productos (nombre_producto),
+	CONSTRAINT FK_id_pedido_pedido_producto FOREIGN KEY  (id_pedido) REFERENCES pedido (id_pedido)
 );
 
-
-		
+	
 create or replace function agregarProducto(
-
 	nombre_pedido varchar(50),
 	precio money,
 	estado int
@@ -119,7 +92,6 @@ BEGIN
 END
 $BODY$
 language plpgsql;
-
 
 		
 create or replace function consultarProducto(
